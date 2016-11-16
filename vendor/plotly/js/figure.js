20e7b9488e82e118041914592492990948368e04
Krishna Singh, Pratap <pratap.krishna.singh@accenture.com>
2016-11-17 01:05:37 +0530

    test

---
+++ vendor/plotly/js/figure.js
@@ -0,0 +1,129 @@
+var figure = {
+    "layout": {
+        "autosize": true, 
+        "yaxis": {
+            "range": [
+                0.5, 
+                4.5
+            ], 
+            "type": "linear", 
+            "autorange": true
+        }, 
+        "title": "Tornado Chart", 
+        "xaxis2": {
+            "domain": [
+                0.5, 
+                1
+            ], 
+            "showticklabels": true, 
+            "autorange": true, 
+            "range": [
+                0, 
+                1100
+            ], 
+            "type": "linear", 
+            "anchor": "y2"
+        }, 
+        "height": 680, 
+        "width": 862, 
+        "yaxis2": {
+            "domain": [
+                0, 
+                1
+            ], 
+            "showticklabels": false, 
+            "autorange": true, 
+            "range": [
+                0.5, 
+                4.5
+            ], 
+            "type": "linear", 
+            "anchor": "x2"
+        }, 
+        "xaxis": {
+            "range": [
+                1100, 
+                0
+            ], 
+            "type": "linear", 
+            "domain": [
+                0, 
+                0.5
+            ], 
+            "autorange": true
+        }, 
+        "showlegend": true
+    }, 
+    "data": [
+        {
+            "autobinx": true, 
+            "autobiny": true, 
+            "orientation": "h", 
+            "name": "Employee Terms", 
+            "y": [
+                "0.5", 
+                "1", 
+                "1.5", 
+                "2",
+                "2.5", 
+                "3", 
+                "3.5", 
+                "4",
+                "4.5", 
+                "5", 
+                "5.5"
+            ], 
+            "x": [
+                "49", 
+                "67", 
+                "140", 
+                "454",
+                "784",
+                "664",
+                "862",
+                "426",
+                "183",
+                "57",
+                "88"
+            ], 
+            "type": "bar", 
+            "uid": "70f09e"
+        }, 
+        {
+            "autobinx": true, 
+            "orientation": "h", 
+            "yaxis": "y2", 
+            "name": "Employee Hires", 
+            "xaxis": "x2", 
+            "y": [
+                "0.5", 
+                "1", 
+                "1.5", 
+                "2",
+                "2.5", 
+                "3", 
+                "3.5", 
+                "4",
+                "4.5", 
+                "5", 
+                "5.5"
+            ], 
+            "x": [
+                "11", 
+                "25", 
+                "223", 
+                "304",
+                "911",
+                "701",
+                "1399",
+                "910",
+                "1017",
+                "268",
+                "288"
+            ], 
+            "autobiny": true, 
+            "type": "bar", 
+            "uid": "85932b"
+        }
+    ]
+}
